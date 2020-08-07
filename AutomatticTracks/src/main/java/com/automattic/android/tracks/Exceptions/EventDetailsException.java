a903ec5d901584d0ee31acaf0de937e8fa1a6558
girl661 <69330509+girl661@users.noreply.github.com>
2020-08-06 20:52:19 -0400

    Delete EventDetailsException.java
    Yo

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/Exceptions/EventDetailsException.java
+++
@@ -1,7 +0,0 @@
-package com.automattic.android.tracks.Exceptions;
-
-public class EventDetailsException extends Exception {
-    public EventDetailsException(String detailMessage) {
-        super(detailMessage);
-    }
-}
